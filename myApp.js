6a872a0bbb454b9c854b0cff0cfd5929a1005681
theNoHoho <yoshihiro_yanagisawa@altee.co>
2020-12-03 09:59:30 +0800

    modify sendfile

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(/views/index.html);
+    res.sendFile(__dirname + "/views/index.html");
   });
 
 
