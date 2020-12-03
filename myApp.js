41d4fb759c381ac5b689a38ad3d806be1455c46c
theNoHoho <yoshihiro_yanagisawa@altee.co>
2020-12-04 07:41:24 +0800

    add app.get jason

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get("/", (req, res) => {
+*app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
   });
 
 app.use(express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+})
 
 
 
