55f401a7e053d6117727a10f2400f68cbd3bd74d
Yana <yana@YMB.local>
2020-12-11 11:19:52 +0800

    modify

--- myApp.js
+++ myApp.js
@@ -18,19 +18,19 @@ app.use(express.static(__dirname + "/public"));
 });*/
 
 app.get( '/json' , function(req,res){
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-    res.json({ 'message': 'HELLO JSON' })
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ "message": "HELLO JSON"})
   }
   else {
-    res.json({ 'message': 'Hello json' })
+    res.json({ "message": "Hello json"})
   }
   });
   
 /*app.get("/json", (req, res) => {
-  if (process.env.MESSAGE_STYLE==='uppercase'){
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
     res.json({"message": "HELLO JSON"})
   }
-  else{
+  else {
     res.json({"message": "Hello json"})
   }
 });*/
