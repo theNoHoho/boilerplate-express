7df120c77dcfb2d48f27e5d18eff5817a346f580
Yana <yana@YMB.local>
2020-12-11 10:51:17 +0800

    modify

--- myApp.js
+++ myApp.js
@@ -17,12 +17,14 @@ app.use(express.static(__dirname + "/public"));
     res.json({"message": "Hello json"})
 });*/
 
-var responsejson = "Hello json"
-if (process.env.MESSAGE_STYLE==='uppercase'){
-responsejson = responsejson.toUpperCase};
 
 app.get("/json", (req, res) => {
-    res.json({"message": responsejson})
+  if (process.env.MESSAGE_STYLE==='uppercase'){
+    res.json({"message": "HELLO JSON"})
+  }
+  else{
+    res.json({"message": "Hello Json"})
+  }
 });
 
 
