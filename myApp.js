44a69b06c36f58f8953dbb1638240424bd0c0076
Yana <yana@YMB.local>
2020-12-11 09:28:20 +0800

    check message_style

--- myApp.js
+++ myApp.js
@@ -13,10 +13,14 @@ app.get("/", (req, res) => {
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) => {
+/*app.get("/json", (req, res) => {
     res.json({"message": "Hello json"})
-});
+});*/
 
+if (process.env.MESSAGE_STYLE=uppercase){
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"})
+})};
 
 
 
