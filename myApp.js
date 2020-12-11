3cbf1c69a270d30d5e86f32f5d77005d41e173e2
Yana <yana@YMB.local>
2020-12-11 08:55:25 +0800

    Hello Express

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ var app = express();
 console.log("Hello World");
 
 app.get("/", (req, res) => {
+  res.send('Hello Express');
+});
+
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
   });
 
