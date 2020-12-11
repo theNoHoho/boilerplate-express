83f8699925d616ed43198856f6d61350fcedc045
Yana <yana@YMB.local>
2020-12-11 08:59:34 +0800

    Hide res-send

--- myApp.js
+++ myApp.js
@@ -3,9 +3,9 @@ var app = express();
 
 console.log("Hello World");
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send('Hello Express');
-});
+});*/
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
